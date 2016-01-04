644037853a17506cf7fac0125533013e5ee7e62b
Pavel Orekhov <vostokpage@gmail.com>
2016-01-04 23:38:08 +1000

    Доработано представление детального просмотра заказа, добавлены выподающие списки выбора статусов заказа, способов доставки и способов оплаты

--- application/controllers/admin/Orders_admin.php
+++ application/controllers/admin/Orders_admin.php
@@ -19,44 +19,52 @@ class Orders_admin extends MY_Controller{
     INNER JOIN payments_methods ON payments_methods.id = orders.payment";
 
     public function index(){
-    //Параметры представления
-    $this->setData('title','Заказы');
+        //Параметры представления
+        $this->setData('title','Заказы');
 
-    //Вывод всех заказов
-    $this->sql .= ' ORDER BY orders.id';
-    $this->setData ('orders',$this->MY_model->get($this->sql));
+        //Вывод всех заказов
+        $this->sql .= ' ORDER BY orders.id';
+        $this->setData ('orders',$this->MY_model->get($this->sql));
 
-    //Вызов отображений
-    $this->load->view('admin/common/header',$this->data);
-    $this->load->view('admin/orders',$this->data);
-    $this->load->view('admin/common/footer');
+        //Вызов отображений
+        $this->load->view('admin/common/header',$this->data);
+        $this->load->view('admin/orders',$this->data);
+        $this->load->view('admin/common/footer');
     }
+
+
     public function editorder() {
 
-    $id = $this->input->get('id');
-    //Получение данных о заказе для редактирования
-    $this->sql .= " WHERE orders.id = $id";
-    $this->setData ('orders',$this->MY_model->get($this->sql));
+        $id = $this->input->get('id');
+        //Получение данных о заказе для редактирования
+        $this->sql .= " WHERE orders.id = $id";
+        $this->setData ('orders',$this->MY_model->get($this->sql));
 
-    //Получение состава заказа
-    $sql2 = "SELECT
-    products.h1 as tovarName,
-    orders_content.count,
-    orders_content.price,
-    orders_content.order_id
-    FROM
-    orders_content
-    INNER JOIN products ON orders_content.product_id = products.id
-    WHERE order_id = $id";
+        //Получение состава заказа
+        $sql2 = "SELECT
+        products.h1 as tovarName,
+        orders_content.count,
+        orders_content.price,
+        orders_content.order_id
+        FROM
+        orders_content
+        INNER JOIN products ON orders_content.product_id = products.id
+        WHERE order_id = $id";
+        $this->setData ('orders_content',$this->MY_model->get($sql2));
 
-    $this->setData ('orders_content',$this->MY_model->get($sql2));
+        //Все статусы заказов
+        $this->setData ('allSatus',$this->MY_model->get_all('orders_status'));
+        //Все способы доставки
+        $this->setData ('allDelivery_methods',$this->MY_model->get_all('delivery_methods'));
+        //Все способы оплаты
+        $this->setData ('allPayments_methods',$this->MY_model->get_all('payments_methods'));
 
-    //Параметры представления
-    $this->setData('title',"Заказ № $id");
-    //Вызов отображений
-    $this->load->view('admin/common/header',$this->data);
-    $this->load->view('admin/ordersDetail',$this->data);
-    $this->load->view('admin/common/footer');
+        //Параметры представления
+        $this->setData('title',"Заказ № $id");
+        //Вызов отображений
+        $this->load->view('admin/common/header',$this->data);
+        $this->load->view('admin/ordersDetail',$this->data);
+        $this->load->view('admin/common/footer');
     }
 }
 ?>
--- application/views/admin/ordersDetail.php
+++ application/views/admin/ordersDetail.php
@@ -1,50 +1,73 @@
 <form action ="" method ="POST" class="form-horizontal" role="form">
     <?foreach ($orders as $order): ?>
-
+<!--        Дата заказа-->
         <div class="form-group">
             <label for="dateReg" class="col-sm-2 control-label">Дата заказа</label>
             <div class="col-md-6">
                 <input type="text" class="form-control" name="dateReg" id="dateReg" value="<? echo $order['date_reg']?>" disabled>
             </div>
         </div>
+<!--        Дата изменения заказа-->
         <div class="form-group">
             <label for="date_update" class="col-sm-2 control-label">Дата изменения заказа</label>
             <div class="col-md-6">
                 <input type="text" class="form-control" name="date_update" id="date_update" value="<? echo $order['date_reg']?>" disabled>
             </div>
         </div>
+<!--        Пользователь-->
         <div class="form-group">
             <label for="Name" class="col-sm-2 control-label">Пользователь</label>
             <div class="col-md-6">
                 <input type="text" class="form-control" name="name" id="Name" value = "<? echo $order['name'].' '.$order['lastname']?>" disabled>
             </div>
         </div>
+<!--        Статус заказа-->
         <div class="form-group">
             <label for="status" class="col-sm-2 control-label">Статус заказа</label>
             <div class="col-md-6">
                 <select class="form-control" name="status" id="status">
-                    <option value="1"><?echo $order['status'];?></option>
+                    <?foreach ($allSatus as $status):
+                        if ($order['status']==$status['name']) {
+                            echo "<option value='$status[id]' selected>".$status['name']."</option>";
+                        }
+                        else {
+                            echo "<option value='$status[id]'>".$status['name']."</option>";}
+                     endforeach ?>
+
                 </select>
             </div>
         </div>
+<!--        Способ доставки-->
         <div class="form-group">
             <label for="delivery" class="col-sm-2 control-label">Способ доставки</label>
             <div class="col-md-6">
                 <select class="form-control" name="delivery" id="delivery">
-                    <option value="1"><?echo $order['delivery'];?></option>
+                    <?foreach ($allDelivery_methods as $delivery_methods):
+                        if ($order['delivery']==$delivery_methods['name']) {
+                            echo "<option value='$delivery_methods[id]' selected>".$delivery_methods['name']."</option>";
+                        }
+                        else {
+                            echo "<option value='$delivery_methods[id]'>".$delivery_methods['name']."</option>";}
+                    endforeach ?>
                 </select>
             </div>
         </div>
+<!--        Способ оплаты-->
         <div class="form-group">
             <label for="payments" class="col-sm-2 control-label">Способ оплаты</label>
             <div class="col-md-6">
                 <select class="form-control" name="payments" id="payments">
-                    <option value="1"><?echo $order['payments'];?></option>
+                    <?foreach ($allPayments_methods as $payments_methods):
+                        if ($order['payments']==$payments_methods['name']) {
+                            echo "<option value='$payments_methods[id]' selected>".$payments_methods['name']."</option>";
+                        }
+                        else {
+                            echo "<option value='$payments_methods[id]'>".$payments_methods['name']."</option>";}
+                    endforeach ?>
                 </select>
             </div>
         </div>
-
-
+<!--            Кнопка-->
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
                 <button type="submit" name="submit" class="btn btn-default">Изменить данные</button>
@@ -52,6 +75,8 @@
         </div>
     <?php endforeach ?>
 </form>
+
+<!--Список товаров-->
 <p class="lead">Состав заказа</p>
 <table class="table table-striped table-bordered table-hover table-condensed">
     <thead>
